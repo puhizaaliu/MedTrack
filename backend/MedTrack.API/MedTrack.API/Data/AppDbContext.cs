c96cb204cd9b5116a7fb8cd557df3ca89d9dd05c
Puhiza Aliu <aliupuhiza@gmail.com>
2025-05-28 18:42:16 +0200

    Konfigurimi i MySQL në projekt - Backend

---
+++ backend/MedTrack.API/MedTrack.API/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MedTrack.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TestEntity> TestEntities { get; set; }
+    }
+
+    public class TestEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- backend/MedTrack.API/MedTrack.API/Program.cs
+++ backend/MedTrack.API/MedTrack.API/Program.cs
@@ -1,3 +1,6 @@
+using MedTrack.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Shto konfigurimin për DbContext (MySQL)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
 
 var app = builder.Build();
 
