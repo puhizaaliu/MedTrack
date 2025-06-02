d83f2cc84a4d7c6af94ddbcecd76ffadfe94ccd5
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:40:43 +0200

    Interface për Appointments - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IAppointmentService.cs
@@ -0,0 +1,19 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IAppointmentService
+    {
+        Task<IEnumerable<Appointment>> GetAllAppointmentsAsync();
+        Task<Appointment?> GetAppointmentByIdAsync(int id);
+        Task AddAppointmentAsync(Appointment appointment);
+        Task UpdateAppointmentAsync(Appointment appointment);
+        Task DeleteAppointmentAsync(int id);
+
+        Task<IEnumerable<Appointment>> GetAppointmentsByDoctorIdAsync(int doctorId);
+        Task<IEnumerable<Appointment>> GetAppointmentsByPatientIdAsync(int patientId);
+        Task<IEnumerable<Appointment>> GetAppointmentsByStatusAsync(AppointmentStatus status);
+    }
+}
