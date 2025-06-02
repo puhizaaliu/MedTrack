69255246774e5678e722f778323ad7ae9814a418
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:48:42 +0200

    Interface për Invoice - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IInvoiceService.cs
@@ -0,0 +1,19 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IInvoiceService
+    {
+        Task<IEnumerable<Invoice>> GetAllInvoicesAsync();
+        Task<Invoice?> GetInvoiceByIdAsync(int id);
+        Task AddInvoiceAsync(Invoice invoice);
+        Task UpdateInvoiceAsync(Invoice invoice);
+        Task DeleteInvoiceAsync(int id);
+
+        Task<IEnumerable<Invoice>> GetInvoicesByPatientIdAsync(int patientId);
+        Task<IEnumerable<Invoice>> GetPaidInvoicesAsync();
+        Task<IEnumerable<Invoice>> GetUnpaidInvoicesAsync();
+    }
+}
