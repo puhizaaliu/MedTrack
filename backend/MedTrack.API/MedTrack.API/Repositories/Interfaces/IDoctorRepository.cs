7e8d173c894a5182bcf2b5a9b6e7d0c12761f7d4
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 18:28:14 +0200

    Interface për Doctor - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,16 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Repositories.Interfaces
+{
+    public interface IDoctorRepository
+    {
+        Task<IEnumerable<Doctor>> GetAllDoctorsAsync();
+        Task<Doctor?> GetDoctorByIdAsync(int id);
+        Task AddDoctorAsync(Doctor doctor);
+        Task UpdateDoctorAsync(Doctor doctor);
+        Task DeleteDoctorAsync(int id);
+        Task<IEnumerable<Doctor>> GetDoctorsBySpecializationIdAsync(int specializationId);
+    }
+}
