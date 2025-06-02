876385b41376e385ce7ac3829b0e2efcf18a8ff7
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 19:49:45 +0200

    Implementation për Specializime të Mjekëve - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Implementations/SpecializationRepository.cs
@@ -0,0 +1,51 @@
+﻿using MedTrack.API.Data;
+using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Repositories.Implementations
+{
+    public class SpecializationRepository : ISpecializationRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SpecializationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Specialization>> GetAllSpecializationsAsync()
+        {
+            return await _context.Specializations.ToListAsync();
+        }
+
+        public async Task<Specialization?> GetSpecializationByIdAsync(int id)
+        {
+            return await _context.Specializations.FindAsync(id);
+        }
+
+        public async Task AddSpecializationAsync(Specialization specialization)
+        {
+            await _context.Specializations.AddAsync(specialization);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateSpecializationAsync(Specialization specialization)
+        {
+            _context.Specializations.Update(specialization);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSpecializationAsync(int id)
+        {
+            var specialization = await _context.Specializations.FindAsync(id);
+            if (specialization != null)
+            {
+                _context.Specializations.Remove(specialization);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
