40a708637e48c7f73707d2d220d49ff799b059ef
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 18:40:52 +0200

    Implementation për Appointments - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Implementations/AppointmentRepository.cs
@@ -0,0 +1,79 @@
+﻿using MedTrack.API.Data;
+using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MedTrack.API.Repositories.Implementations
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AppointmentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Appointment>> GetAllAppointmentsAsync()
+        {
+            return await _context.Appointments
+                .Include(a => a.Doctor)
+                .Include(a => a.Patient)
+                .ToListAsync();
+        }
+
+        public async Task<Appointment?> GetAppointmentByIdAsync(int id)
+        {
+            return await _context.Appointments
+                .Include(a => a.Doctor)
+                .Include(a => a.Patient)
+                .FirstOrDefaultAsync(a => a.AppointmentId == id);
+        }
+
+        public async Task AddAppointmentAsync(Appointment appointment)
+        {
+            await _context.Appointments.AddAsync(appointment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAppointmentAsync(Appointment appointment)
+        {
+            _context.Appointments.Update(appointment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAppointmentAsync(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment != null)
+            {
+                _context.Appointments.Remove(appointment);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Appointment>> GetAppointmentsByDoctorIdAsync(int doctorId)
+        {
+            return await _context.Appointments
+                .Where(a => a.DoctorId == doctorId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Appointment>> GetAppointmentsByPatientIdAsync(int patientId)
+        {
+            return await _context.Appointments
+                .Where(a => a.PatientId == patientId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Appointment>> GetAppointmentsByStatusAsync(AppointmentStatus status)
+        {
+            return await _context.Appointments
+                .Where(a => a.Status == status)
+                .ToListAsync();
+        }
+    }
+}
