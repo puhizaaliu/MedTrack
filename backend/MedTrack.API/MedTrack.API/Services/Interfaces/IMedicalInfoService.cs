2e0243fdca4d75dea15ee57b7901b5b82d938efb
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:52:52 +0200

    Interface për Medical Info të Pacientëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IMedicalInfoService.cs
@@ -0,0 +1,13 @@
+﻿using MedTrack.API.Models;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IMedicalInfoService
+    {
+        Task<MedicalInfo?> GetMedicalInfoByPatientIdAsync(int patientId);
+        Task AddMedicalInfoAsync(MedicalInfo medicalInfo);
+        Task UpdateMedicalInfoAsync(MedicalInfo medicalInfo);
+        Task DeleteMedicalInfoAsync(int id);
+    }
+}
