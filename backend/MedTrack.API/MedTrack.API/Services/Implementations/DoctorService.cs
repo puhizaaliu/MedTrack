5b280658d76e807b8cf87ee36436404493b73cbf
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:23:07 +0200

    Implementation për Doctor - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Implementations/DoctorService.cs
@@ -0,0 +1,48 @@
+﻿using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Implementations
+{
+    public class DoctorService : IDoctorService
+    {
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public async Task<IEnumerable<Doctor>> GetAllDoctorsAsync()
+        {
+            return await _doctorRepository.GetAllDoctorsAsync();
+        }
+
+        public async Task<Doctor?> GetDoctorByIdAsync(int id)
+        {
+            return await _doctorRepository.GetDoctorByIdAsync(id);
+        }
+
+        public async Task AddDoctorAsync(Doctor doctor)
+        {
+            await _doctorRepository.AddDoctorAsync(doctor);
+        }
+
+        public async Task UpdateDoctorAsync(Doctor doctor)
+        {
+            await _doctorRepository.UpdateDoctorAsync(doctor);
+        }
+
+        public async Task DeleteDoctorAsync(int id)
+        {
+            await _doctorRepository.DeleteDoctorAsync(id);
+        }
+
+        public async Task<IEnumerable<Doctor>> GetDoctorsBySpecializationIdAsync(int specializationId)
+        {
+            return await _doctorRepository.GetDoctorsBySpecializationIdAsync(specializationId);
+        }
+    }
+}
