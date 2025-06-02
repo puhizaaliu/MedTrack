4079a53e9f39859b743ec76467614f61156001c9
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 21:22:13 +0200

    Konfigurimi i dependency injection për Services

--- backend/MedTrack.API/MedTrack.API/Program.cs
+++ backend/MedTrack.API/MedTrack.API/Program.cs
@@ -1,16 +1,41 @@
-using MedTrack.API.Data;
-using Microsoft.EntityFrameworkCore;
 using MedTrack.API.Config;
-using MongoDB.Driver;
+using MedTrack.API.Data;
+using MedTrack.API.Repositories.Implementations;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Implementations;
+using MedTrack.API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using MongoServerVersion = MongoDB.Driver.ServerVersion;
 using ServerVersion = Microsoft.EntityFrameworkCore.ServerVersion;
 
 var builder = WebApplication.CreateBuilder(args);
+// Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+builder.Services.AddScoped<IMedicalInfoRepository, MedicalInfoRepository>();
+builder.Services.AddScoped<IFamilyHistoryRepository, FamilyHistoryRepository>();
+builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
+builder.Services.AddScoped<ISpecializationRepository, SpecializationRepository>();
+
+// Services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+builder.Services.AddScoped<IMedicalInfoService, MedicalInfoService>();
+builder.Services.AddScoped<IFamilyHistoryService, FamilyHistoryService>();
+builder.Services.AddScoped<IServiceService, ServiceService>();
+builder.Services.AddScoped<ISpecializationService, SpecializationService>();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
