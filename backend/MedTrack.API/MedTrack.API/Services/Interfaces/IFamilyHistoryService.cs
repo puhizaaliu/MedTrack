78b186d5aaede4c71c42f95c04956fcfbabd8795
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:55:17 +0200

    Interface për Family History të Pacientëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IFamilyHistoryService.cs
@@ -0,0 +1,15 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IFamilyHistoryService
+    {
+        Task<IEnumerable<FamilyHistory>> GetAllFamilyHistoriesAsync();
+        Task<FamilyHistory?> GetFamilyHistoryByIdAsync(int id);
+        Task AddFamilyHistoryAsync(FamilyHistory familyHistory);
+        Task UpdateFamilyHistoryAsync(FamilyHistory familyHistory);
+        Task DeleteFamilyHistoryAsync(int id);
+    }
+}
