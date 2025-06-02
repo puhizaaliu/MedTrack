a62188355311af6b1c101a06492339ba9554f2fa
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 18:33:45 +0200

    Implementation për Patients - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Implementations/PatientRepository.cs
@@ -0,0 +1,53 @@
+﻿using MedTrack.API.Data;
+using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MedTrack.API.Repositories.Implementations
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PatientRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Patient>> GetAllPatientsAsync()
+        {
+            // Mund të përfshijë edhe info të tjera të lidhura, nëse ka (p.sh. MedicalInfos)
+            return await _context.Patients.ToListAsync();
+        }
+
+        public async Task<Patient?> GetPatientByIdAsync(int id)
+        {
+            return await _context.Patients.FindAsync(id);
+        }
+
+        public async Task AddPatientAsync(Patient patient)
+        {
+            await _context.Patients.AddAsync(patient);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdatePatientAsync(Patient patient)
+        {
+            _context.Patients.Update(patient);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePatientAsync(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient != null)
+            {
+                _context.Patients.Remove(patient);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
