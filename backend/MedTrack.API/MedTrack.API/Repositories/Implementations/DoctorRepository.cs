3dc705ab125ca2bb4202f047264a993a88d50afc
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 18:28:24 +0200

    Implementation për Doctor - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Implementations/DoctorRepository.cs
@@ -0,0 +1,64 @@
+﻿using MedTrack.API.Data;
+using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MedTrack.API.Repositories.Implementations
+{
+    public class DoctorRepository : IDoctorRepository
+    {
+        private readonly AppDbContext _context;
+
+        public DoctorRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Doctor>> GetAllDoctorsAsync()
+        {
+            
+            return await _context.Doctors
+                .Include(d => d.Specialization)
+                .ToListAsync();
+        }
+
+        public async Task<Doctor?> GetDoctorByIdAsync(int id)
+        {
+            return await _context.Doctors
+                .Include(d => d.Specialization)
+                .FirstOrDefaultAsync(d => d.UserId == id);
+        }
+
+        public async Task AddDoctorAsync(Doctor doctor)
+        {
+            await _context.Doctors.AddAsync(doctor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateDoctorAsync(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteDoctorAsync(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Doctor>> GetDoctorsBySpecializationIdAsync(int specializationId)
+        {
+            return await _context.Doctors
+                .Where(d => d.SpecializationId == specializationId)
+                .ToListAsync();
+        }
+    }
+}
