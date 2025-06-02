e1492b0efe5449b86aa8e1db5062546bc3b11ed6
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:55:53 +0200

    Implementation për Family History të Pacientëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Implementations/FamilyHistoryService.cs
@@ -0,0 +1,43 @@
+﻿using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Implementations
+{
+    public class FamilyHistoryService : IFamilyHistoryService
+    {
+        private readonly IFamilyHistoryRepository _familyHistoryRepository;
+
+        public FamilyHistoryService(IFamilyHistoryRepository familyHistoryRepository)
+        {
+            _familyHistoryRepository = familyHistoryRepository;
+        }
+
+        public async Task<IEnumerable<FamilyHistory>> GetAllFamilyHistoriesAsync()
+        {
+            return await _familyHistoryRepository.GetAllFamilyHistoriesAsync();
+        }
+
+        public async Task<FamilyHistory?> GetFamilyHistoryByIdAsync(int id)
+        {
+            return await _familyHistoryRepository.GetFamilyHistoryByIdAsync(id);
+        }
+
+        public async Task AddFamilyHistoryAsync(FamilyHistory familyHistory)
+        {
+            await _familyHistoryRepository.AddFamilyHistoryAsync(familyHistory);
+        }
+
+        public async Task UpdateFamilyHistoryAsync(FamilyHistory familyHistory)
+        {
+            await _familyHistoryRepository.UpdateFamilyHistoryAsync(familyHistory);
+        }
+
+        public async Task DeleteFamilyHistoryAsync(int id)
+        {
+            await _familyHistoryRepository.DeleteFamilyHistoryAsync(id);
+        }
+    }
+}
