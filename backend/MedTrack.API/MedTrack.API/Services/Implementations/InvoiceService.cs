88fadd9e5b0c134bb4e902f0036a0186834cc451
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:49:38 +0200

    Implementation për Invoice - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Implementations/InvoiceService.cs
@@ -0,0 +1,58 @@
+﻿using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Implementations
+{
+    public class InvoiceService : IInvoiceService
+    {
+        private readonly IInvoiceRepository _invoiceRepository;
+
+        public InvoiceService(IInvoiceRepository invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+
+        public async Task<IEnumerable<Invoice>> GetAllInvoicesAsync()
+        {
+            return await _invoiceRepository.GetAllInvoicesAsync();
+        }
+
+        public async Task<Invoice?> GetInvoiceByIdAsync(int id)
+        {
+            return await _invoiceRepository.GetInvoiceByIdAsync(id);
+        }
+
+        public async Task AddInvoiceAsync(Invoice invoice)
+        {
+            await _invoiceRepository.AddInvoiceAsync(invoice);
+        }
+
+        public async Task UpdateInvoiceAsync(Invoice invoice)
+        {
+            await _invoiceRepository.UpdateInvoiceAsync(invoice);
+        }
+
+        public async Task DeleteInvoiceAsync(int id)
+        {
+            await _invoiceRepository.DeleteInvoiceAsync(id);
+        }
+
+        public async Task<IEnumerable<Invoice>> GetInvoicesByPatientIdAsync(int patientId)
+        {
+            return await _invoiceRepository.GetInvoicesByPatientIdAsync(patientId);
+        }
+
+        public async Task<IEnumerable<Invoice>> GetPaidInvoicesAsync()
+        {
+            return await _invoiceRepository.GetPaidInvoicesAsync();
+        }
+
+        public async Task<IEnumerable<Invoice>> GetUnpaidInvoicesAsync()
+        {
+            return await _invoiceRepository.GetUnpaidInvoicesAsync();
+        }
+    }
+}
