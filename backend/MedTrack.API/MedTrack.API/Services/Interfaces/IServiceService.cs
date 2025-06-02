ffa892248d72ff2a6ec03987d87080a3ee5c1d2a
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:58:15 +0200

    Interface për Services të Mjekëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IServiceService.cs
@@ -0,0 +1,15 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IServiceService
+    {
+        Task<IEnumerable<Service>> GetAllServicesAsync();
+        Task<Service?> GetServiceByIdAsync(int id);
+        Task AddServiceAsync(Service service);
+        Task UpdateServiceAsync(Service service);
+        Task DeleteServiceAsync(int id);
+    }
+}
