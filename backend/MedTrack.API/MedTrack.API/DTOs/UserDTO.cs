b9e4d51866ba129411ff027c78ee6619c1bc13d5
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 21:58:52 +0200

    Krijmi i UserDTO

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using MedTrack.API.Models;
+
+namespace MedTrack.API.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string ParentName { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+    }
+}
