7ea778e383d14b5e669996455eab2b6c08d98999
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:30:16 +0200

    Interface për Patients - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IPatientService.cs
@@ -0,0 +1,15 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IPatientService
+    {
+        Task<IEnumerable<Patient>> GetAllPatientsAsync();
+        Task<Patient?> GetPatientByIdAsync(int id);
+        Task AddPatientAsync(Patient patient);
+        Task UpdatePatientAsync(Patient patient);
+        Task DeletePatientAsync(int id);
+    }
+}
