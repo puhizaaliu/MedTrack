0699110837f5b5a8ced7867b77ca65e298c9e7e4
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 18:44:38 +0200

    Implementation për Invoices - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Implementations/InvoiceRepository.cs
@@ -0,0 +1,80 @@
+﻿using MedTrack.API.Data;
+using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MedTrack.API.Repositories.Implementations
+{
+    public class InvoiceRepository : IInvoiceRepository
+    {
+        private readonly AppDbContext _context;
+
+        public InvoiceRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Invoice>> GetAllInvoicesAsync()
+        {
+            return await _context.Invoices
+                .Include(i => i.Appointment)
+                .ThenInclude(a => a.Patient)
+                .ToListAsync();
+        }
+
+        public async Task<Invoice?> GetInvoiceByIdAsync(int id)
+        {
+            return await _context.Invoices
+                .Include(i => i.Appointment)
+                .ThenInclude(a => a.Patient)
+                .FirstOrDefaultAsync(i => i.InvoiceId == id);
+        }
+
+        public async Task AddInvoiceAsync(Invoice invoice)
+        {
+            await _context.Invoices.AddAsync(invoice);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateInvoiceAsync(Invoice invoice)
+        {
+            _context.Invoices.Update(invoice);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteInvoiceAsync(int id)
+        {
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice != null)
+            {
+                _context.Invoices.Remove(invoice);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Invoice>> GetInvoicesByPatientIdAsync(int patientId)
+        {
+            return await _context.Invoices
+                .Include(i => i.Appointment)
+                .Where(i => i.Appointment.PatientId == patientId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Invoice>> GetPaidInvoicesAsync()
+        {
+            return await _context.Invoices
+                .Where(i => i.PaymentStatus == true)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Invoice>> GetUnpaidInvoicesAsync()
+        {
+            return await _context.Invoices
+                .Where(i => i.PaymentStatus == false)
+                .ToListAsync();
+        }
+    }
+}
