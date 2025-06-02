d2954ff5d624f134f3d1db92bc12718c9ba5f8f7
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 18:31:00 +0200

    Interface për Patients - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Interfaces/IPatientRepository.cs
@@ -0,0 +1,15 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Repositories.Interfaces
+{
+    public interface IPatientRepository
+    {
+        Task<IEnumerable<Patient>> GetAllPatientsAsync();
+        Task<Patient?> GetPatientByIdAsync(int id);
+        Task AddPatientAsync(Patient patient);
+        Task UpdatePatientAsync(Patient patient);
+        Task DeletePatientAsync(int id);
+    }
+}
