d4097da10d6d6a87c023732899c70535de2effae
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:33:46 +0200

    Implementation për Patients - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Implementations/PatientService.cs
@@ -0,0 +1,43 @@
+﻿using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Implementations
+{
+    public class PatientService : IPatientService
+    {
+        private readonly IPatientRepository _patientRepository;
+
+        public PatientService(IPatientRepository patientRepository)
+        {
+            _patientRepository = patientRepository;
+        }
+
+        public async Task<IEnumerable<Patient>> GetAllPatientsAsync()
+        {
+            return await _patientRepository.GetAllPatientsAsync();
+        }
+
+        public async Task<Patient?> GetPatientByIdAsync(int id)
+        {
+            return await _patientRepository.GetPatientByIdAsync(id);
+        }
+
+        public async Task AddPatientAsync(Patient patient)
+        {
+            await _patientRepository.AddPatientAsync(patient);
+        }
+
+        public async Task UpdatePatientAsync(Patient patient)
+        {
+            await _patientRepository.UpdatePatientAsync(patient);
+        }
+
+        public async Task DeletePatientAsync(int id)
+        {
+            await _patientRepository.DeletePatientAsync(id);
+        }
+    }
+}
