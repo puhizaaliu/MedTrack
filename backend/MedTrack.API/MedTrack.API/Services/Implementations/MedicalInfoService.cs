de3f699bbfac6db8703f587874f164a0a279a2d7
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:53:39 +0200

    Implementation për Medical Info të Pacientëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Implementations/MedicalInfoService.cs
@@ -0,0 +1,37 @@
+﻿using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Implementations
+{
+    public class MedicalInfoService : IMedicalInfoService
+    {
+        private readonly IMedicalInfoRepository _medicalInfoRepository;
+
+        public MedicalInfoService(IMedicalInfoRepository medicalInfoRepository)
+        {
+            _medicalInfoRepository = medicalInfoRepository;
+        }
+
+        public async Task<MedicalInfo?> GetMedicalInfoByPatientIdAsync(int patientId)
+        {
+            return await _medicalInfoRepository.GetMedicalInfoByPatientIdAsync(patientId);
+        }
+
+        public async Task AddMedicalInfoAsync(MedicalInfo medicalInfo)
+        {
+            await _medicalInfoRepository.AddMedicalInfoAsync(medicalInfo);
+        }
+
+        public async Task UpdateMedicalInfoAsync(MedicalInfo medicalInfo)
+        {
+            await _medicalInfoRepository.UpdateMedicalInfoAsync(medicalInfo);
+        }
+
+        public async Task DeleteMedicalInfoAsync(int id)
+        {
+            await _medicalInfoRepository.DeleteMedicalInfoAsync(id);
+        }
+    }
+}
