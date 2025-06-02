bf8d0ad62e0ff8c617e34a4727d8cb78e7345306
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 21:05:29 +0200

    Interface për Specializime të Mjekëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/ISpecializationService.cs
@@ -0,0 +1,15 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface ISpecializationService
+    {
+        Task<IEnumerable<Specialization>> GetAllSpecializationsAsync();
+        Task<Specialization?> GetSpecializationByIdAsync(int id);
+        Task AddSpecializationAsync(Specialization specialization);
+        Task UpdateSpecializationAsync(Specialization specialization);
+        Task DeleteSpecializationAsync(int id);
+    }
+}
