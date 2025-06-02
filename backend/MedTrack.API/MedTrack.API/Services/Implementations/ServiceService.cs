21edbe59159bdf2c6d6d130535248b627825974c
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 21:00:04 +0200

    Implementation për Services të Mjekëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Implementations/ServiceService.cs
@@ -0,0 +1,43 @@
+﻿using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Implementations
+{
+    public class ServiceService : IServiceService
+    {
+        private readonly IServiceRepository _serviceRepository;
+
+        public ServiceService(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+
+        public async Task<IEnumerable<Service>> GetAllServicesAsync()
+        {
+            return await _serviceRepository.GetAllServicesAsync();
+        }
+
+        public async Task<Service?> GetServiceByIdAsync(int id)
+        {
+            return await _serviceRepository.GetServiceByIdAsync(id);
+        }
+
+        public async Task AddServiceAsync(Service service)
+        {
+            await _serviceRepository.AddServiceAsync(service);
+        }
+
+        public async Task UpdateServiceAsync(Service service)
+        {
+            await _serviceRepository.UpdateServiceAsync(service);
+        }
+
+        public async Task DeleteServiceAsync(int id)
+        {
+            await _serviceRepository.DeleteServiceAsync(id);
+        }
+    }
+}
