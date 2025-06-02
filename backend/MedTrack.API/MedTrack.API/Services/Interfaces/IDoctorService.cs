58984d4ea822ed094b48ae713e496bc3bfb049d7
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:14:07 +0200

    Interface për Doctor - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IDoctorService.cs
@@ -0,0 +1,16 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<Doctor>> GetAllDoctorsAsync();
+        Task<Doctor?> GetDoctorByIdAsync(int id);
+        Task AddDoctorAsync(Doctor doctor);
+        Task UpdateDoctorAsync(Doctor doctor);
+        Task DeleteDoctorAsync(int id);
+        Task<IEnumerable<Doctor>> GetDoctorsBySpecializationIdAsync(int specializationId);
+    }
+}
