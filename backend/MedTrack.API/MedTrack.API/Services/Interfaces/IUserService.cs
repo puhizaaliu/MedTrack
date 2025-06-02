c57498b8dcdce9b1a8bfa931f2ac4a947b5da32e
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 20:04:53 +0200

    Interface për User - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using MedTrack.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
