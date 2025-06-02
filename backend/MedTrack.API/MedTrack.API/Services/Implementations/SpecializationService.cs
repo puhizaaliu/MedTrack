3c498a434904c52e46bbadaff06aff0c37a09f4a
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 21:06:19 +0200

    Implementation për Specializime të Mjekëve - Service

---
+++ backend/MedTrack.API/MedTrack.API/Services/Implementations/SpecializationService.cs
@@ -0,0 +1,43 @@
+﻿using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using MedTrack.API.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Services.Implementations
+{
+    public class SpecializationService : ISpecializationService
+    {
+        private readonly ISpecializationRepository _specializationRepository;
+
+        public SpecializationService(ISpecializationRepository specializationRepository)
+        {
+            _specializationRepository = specializationRepository;
+        }
+
+        public async Task<IEnumerable<Specialization>> GetAllSpecializationsAsync()
+        {
+            return await _specializationRepository.GetAllSpecializationsAsync();
+        }
+
+        public async Task<Specialization?> GetSpecializationByIdAsync(int id)
+        {
+            return await _specializationRepository.GetSpecializationByIdAsync(id);
+        }
+
+        public async Task AddSpecializationAsync(Specialization specialization)
+        {
+            await _specializationRepository.AddSpecializationAsync(specialization);
+        }
+
+        public async Task UpdateSpecializationAsync(Specialization specialization)
+        {
+            await _specializationRepository.UpdateSpecializationAsync(specialization);
+        }
+
+        public async Task DeleteSpecializationAsync(int id)
+        {
+            await _specializationRepository.DeleteSpecializationAsync(id);
+        }
+    }
+}
