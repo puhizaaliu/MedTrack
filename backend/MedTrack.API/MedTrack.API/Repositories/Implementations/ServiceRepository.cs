ddf8b46066a7e5adccc909815251c9757570a40c
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-02 19:39:44 +0200

    Implementation për Services të Mjekëve - Repository

---
+++ backend/MedTrack.API/MedTrack.API/Repositories/Implementations/ServiceRepository.cs
@@ -0,0 +1,51 @@
+﻿using MedTrack.API.Data;
+using MedTrack.API.Models;
+using MedTrack.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedTrack.API.Repositories.Implementations
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Service>> GetAllServicesAsync()
+        {
+            return await _context.Services.ToListAsync();
+        }
+
+        public async Task<Service?> GetServiceByIdAsync(int id)
+        {
+            return await _context.Services.FindAsync(id);
+        }
+
+        public async Task AddServiceAsync(Service service)
+        {
+            await _context.Services.AddAsync(service);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateServiceAsync(Service service)
+        {
+            _context.Services.Update(service);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteServiceAsync(int id)
+        {
+            var service = await _context.Services.FindAsync(id);
+            if (service != null)
+            {
+                _context.Services.Remove(service);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
