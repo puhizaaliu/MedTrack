aa0b40aeb5f912693955a40407cf0c2dc4bee56c
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-03 14:52:48 +0200

    Krijimi i CreateInvoiceDTO dhe UpdateInvoiceDTO

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/CreateInvoiceDTO.cs
@@ -0,0 +1,11 @@
+﻿using MedTrack.API.Models;
+
+namespace MedTrack.API.DTOs
+{
+    public class CreateInvoiceDTO
+    {
+        public int AppointmentId { get; set; }
+        public decimal Amount { get; set; }
+        public PaymentMethod Method { get; set; }
+    }
+}
---
+++ backend/MedTrack.API/MedTrack.API/DTOs/UpdateInvoiceDTO.cs
@@ -0,0 +1,10 @@
+﻿using MedTrack.API.Models;
+
+namespace MedTrack.API.DTOs
+{
+    public class UpdateInvoiceDTO
+    {
+        public PaymentMethod Method { get; set; }
+        public bool PaymentStatus { get; set; }
+    }
+}
