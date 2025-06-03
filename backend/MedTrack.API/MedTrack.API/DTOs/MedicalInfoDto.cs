768bbc8b273278ceae205c26ecccec2390dd65d8
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-03 14:24:09 +0200

    Krijimi i MedicalInfoDTO

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/MedicalInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MedTrack.API.DTOs
+{
+    public class MedicalInfoDto
+    {
+        public string Allergies { get; set; } = string.Empty;
+        public string Medications { get; set; } = string.Empty;
+        public bool Smoking { get; set; }
+        public bool Alcohol { get; set; }
+        public string PhysicalActivity { get; set; } = string.Empty;
+    }
+}
