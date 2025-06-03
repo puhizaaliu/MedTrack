13cb919433c46d52ce694d697a49ca0049cf5429
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-03 14:39:42 +0200

    krijimi i CreateMedicalInfoDTO dhe UpdateMedicalInfoDTO

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/CreateMedicalInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MedTrack.API.DTOs
+{
+    public class CreateMedicalInfoDTO
+    {
+        public int UserId { get; set; } // lidhet me userin
+        public string Allergies { get; set; } = string.Empty;
+        public string Medications { get; set; } = string.Empty;
+        public bool Smoking { get; set; }
+        public bool Alcohol { get; set; }
+        public string PhysicalActivity { get; set; } = string.Empty;
+    }
+}
