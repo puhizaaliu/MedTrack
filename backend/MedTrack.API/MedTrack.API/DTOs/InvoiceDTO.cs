0ee5d481062f31b75525388ecf6e61738c442f48
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-03 14:51:26 +0200

    Krijimi i InvoiceDTO

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/InvoiceDTO.cs
@@ -0,0 +1,15 @@
+﻿using MedTrack.API.Models;
+
+namespace MedTrack.API.DTOs
+{
+    public class InvoiceDTO
+    {
+        public int InvoiceId { get; set; }
+        public int AppointmentId { get; set; }
+        public string PatientName { get; set; } = string.Empty;
+        public string DoctorName { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public PaymentMethod Method { get; set; }
+        public bool PaymentStatus { get; set; }
+    }
+}
