6401a7cb02baf1e53fffac2322945084d339929b
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-06 22:26:51 +0200

    Krijimi i DTO-ve për Notifications

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/NotificationDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MedTrack.API.DTOs
+{
+    public class NotificationDto
+    {
+        public string Id { get; set; } = null!;            
+        public int UserId { get; set; }                   
+        public string Type { get; set; } = null!;          
+        public string Message { get; set; } = null!;      
+        public int? AppointmentId { get; set; }           
+        public string? MedicalReportId { get; set; }       
+        public bool IsRead { get; set; }                   
+        public DateTime CreatedAt { get; set; }           
+        public DateTime? ReadAt { get; set; }              
+    }
+}
---
+++ backend/MedTrack.API/MedTrack.API/DTOs/UpdateNotificationDTO.cs
@@ -0,0 +1,16 @@
+﻿
+using MedTrack.API.MongoModels;
+using System;
+
+namespace MedTrack.API.DTOs
+{
+    public class UpdateNotificationDto
+    {
+        public string? Type { get; set; }                   
+        public string? Message { get; set; }                
+        public int? AppointmentId { get; set; }           
+        public string? MedicalReportId { get; set; }      
+        public bool? IsRead { get; set; }                 
+        public DateTime? ReadAt { get; set; }              
+    }
+}
