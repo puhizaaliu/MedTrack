6b9474a3ed3495897a3e480c01b0c55eccf793bb
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-07 16:09:53 +0200

    Krijimi i Modelit RefreshTokenRequest

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/Auth/RefreshTokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedTrack.API.Dtos.Auth
+{
+    public class RefreshTokenRequest
+    {
+        [Required]
+        public string Token { get; set; } = null!;
+    }
+}
