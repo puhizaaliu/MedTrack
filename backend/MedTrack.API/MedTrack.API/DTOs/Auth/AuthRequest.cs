892d831f17da264e0d1844cfa3162e57bc8f7047
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-07 16:07:21 +0200

    Krijimi i Modelit AuthRequest

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/Auth/AuthRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedTrack.API.Dtos.Auth
+{
+    public class AuthRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
