a56fa1d8b76d5fe7a7683f8125d41effb4c2db3c
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-07 16:04:21 +0200

    Krijimi i Modelit RefreshToken

---
+++ backend/MedTrack.API/MedTrack.API/Models/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedTrack.API.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Token { get; set; } = null!;
+
+        public DateTime Created { get; set; }
+        public DateTime Expires { get; set; }
+        public DateTime? Revoked { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
