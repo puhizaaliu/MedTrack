cacfdddacdf1adf4486f7f71c8897f9421c72e7c
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-07 16:08:49 +0200

    Krijimi i Modelit AuthRespose

---
+++ backend/MedTrack.API/MedTrack.API/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MedTrack.API.Dtos.Auth
+{
+    public class AuthResponse
+    {
+        public string AccessToken { get; set; } = null!;
+        public DateTime AccessTokenExpires { get; set; }
+        public string RefreshToken { get; set; } = null!;
+        // Opsional: User info
+        public int UserId { get; set; }
+        public string Role { get; set; } = null!;
+    }
+}
