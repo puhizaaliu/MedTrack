5ece46279aacf2b36f8536b387e25b3ec04753c0
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-08 12:58:07 +0200

    Krijimi i AuthorizeRolesAttribute

---
+++ backend/MedTrack.API/MedTrack.API/Attributes/AuthorizeRolesAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using MedTrack.API.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MedTrack.API.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        public AuthorizeRolesAttribute(params UserRole[] roles)
+        {
+            if (roles == null || roles.Length == 0)
+                throw new ArgumentException("At least one role must be specified.", nameof(roles));
+
+            // Convert enum values to comma-separated string
+            Roles = string.Join(",", roles.Select(r => r.ToString()));
+        }
+    }
+}
