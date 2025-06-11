9b90ea75264b24c588fef1d4071e8fea5b92021f
Puhiza Aliu <aliupuhiza@gmail.com>
2025-06-11 19:16:17 +0200

    Konfigurimi i Vite dhe Tailwind në Projekt

--- frontend/medtrack-frontend/src/pages/Common/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Welcome to MedTrack</h1>
-      <p>Menaxhimi elektronik i termi ne klinike, shpejt dhe lehtë.</p>
-    </div>
-  );
-}
